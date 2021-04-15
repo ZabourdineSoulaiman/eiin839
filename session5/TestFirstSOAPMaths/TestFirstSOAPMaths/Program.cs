4aec8fd8746d8f1afe0c2e29ffa0f8c1b97a6802
Soulaiman Zabourdine <mohamedsoulaiman-tha.zabourdine@etu.univ-cotedazur.fr>
2021-04-15 11:46:43 +0200

    Added work done for session 5

---
+++ session5/TestFirstSOAPMaths/TestFirstSOAPMaths/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFirstSOAPMaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMaths.MathsOperationsClient mathClient = new MyMaths.MathsOperationsClient();
+            int result = mathClient.Add(5, 100);
+            Console.WriteLine("result : " + result);
+            Console.ReadLine();
+        }
+    }
+}
